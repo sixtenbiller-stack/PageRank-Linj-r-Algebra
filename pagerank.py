f76bc3e5c607d3a7dcdcbbfba39c60f0637a2739
Sixten <sixtenbiller@gmail.com>
2026-02-16 19:35:56 +0100

    Lade till exempelfil och lade till komentarer i koden

--- pagerank.py
+++ pagerank.py
@@ -3,34 +3,34 @@ import random
 class hemsida:
     def __init__(self, nr, namn, länkningar,d,N): 
         self.namn = namn
-        self.nr = nr
+        self.nr = nr #Unikt nr id
         self.pageRankVärde = (1-d)/N
         self.GammaltPageRankVärde = (1-d)/N
-        self.länkningar = länkningar
-        self.d = d
-        self.N = N
+        self.länkningar = länkningar #Lista med sidor den länkar till
+        self.d = d # Dämpningsfaktor, asigneras i mainfunktionen vid initiering av varje objekt
+        self.N = N #Totala antalet sidor
 
     def __str__(self):
-        string = f'{self.namn} har PageRank-Värdet: {round(self.GammaltPageRankVärde,5)} och länkar till {len(self.länkningar)} andra sidor'
+        string = f'{self.namn} har PageRank-Värdet: {round(self.GammaltPageRankVärde,7)} och länkar till {len(self.länkningar)} andra sidor'
         return string
-
         
 #Länkningar är bara en lista med massa nummer som korresponderar till sidor den är länkad till
-    def länkning(self,sidlista): 
+    def länkning(self,sidlista):
+        länkVärde = (self.GammaltPageRankVärde/len(self.länkningar))*self.d #Beräknar värdet av varje av sidans länkning
         for i in self.länkningar:
-            sidlista[i-1].pageRankVärde += (self.GammaltPageRankVärde/len(self.länkningar))*self.d
+            sidlista[i-1].pageRankVärde += länkVärde #i-1 efterssom python har 0-indexering
 
-    def nollställning(self):
+    def nollställning(self): #Funktion för att nollställa mellan iterationerna
         self.GammaltPageRankVärde = self.pageRankVärde
         self.pageRankVärde = (1-self.d)/self.N
 
-def LasInFil(filnamn, d):
+
+def LasInFil(filnamn, d): #Funktion för att läsa in ett "nät" från en text-fil för att köra page-rank på
     sidlista = []
     antalsidor = 0
-    print("läs in från fil startad")
     with open(str(filnamn),'r',encoding='utf-8',) as f:
         f.readline()
-        while True:
+        while True: #Körs tills listan tar slut, inte jättebra sätt men fungerar
             try:
                 line = f.readline()
                 linelist = line.split(',')
@@ -39,7 +39,7 @@ def LasInFil(filnamn, d):
                 
                 linelist[2] = linelist[2].replace('\n','')
                 länkningar = linelist[2].split(':')
-                länkningar = [int(länkning) for länkning in länkningar]
+                länkningar = [int(länkning) for länkning in länkningar] #Konverterar länkningar till lista av intigers
                 sidlista.append(hemsida(int(linelist[0]),str(linelist[1]),länkningar,d,1))
 
                 antalsidor += 1
@@ -48,18 +48,20 @@ def LasInFil(filnamn, d):
                 print(e)
                 break
 
-    for i in sidlista:
-        i.N = antalsidor
-        i.nollställning()
-        i.nollställning()
+    for sida in sidlista: #Nollställer varje sida 2 gånger för att de ska räkna med korrekt värde för N
+        sida.N = antalsidor
+        sida.nollställning()
+        sida.nollställning()
         
     return sidlista
 
-def skapaRandomSidor(d, N):
+
+def skapaRandomSidor(d, N): #Fungerar knappt, rekomenderas inte
     sidlista = []
     for i in range(N):
         sidlista.append(hemsida(i,"lebronjames.com",[random.randint(1,N),random.randint(1,N),random.randint(1,N)],d,N))
     return sidlista
+
 
 def kontrollSumma(sidlista):
     summa = 0
@@ -67,21 +69,26 @@ def kontrollSumma(sidlista):
         summa += i.GammaltPageRankVärde
     return summa
 
+
 def main():
 
     N = 10
     d = 0.85
+    SorteraLista = True #Om True kommer sidorna med högst PageRank-Värde hamna högst upp
     
-    sidlista = LasInFil("test.txt",d)
+    sidlista = LasInFil("SidorExempel.txt",d)
 
-    for i in range(1000):
+    for i in range(1000): #Kör 1000 iterationer
         for i in sidlista:
             i.länkning(sidlista)
         for i in sidlista:
             i.nollställning()
 
+    if SorteraLista == True:
+        sidlista.sort(key=lambda sida: sida.GammaltPageRankVärde, reverse=True)
+    
     for i in sidlista:
         print(i)
-    print("Summan av alla PageRank-Värden är: ",kontrollSumma(sidlista))
+    print("Summan av alla PageRank-Värden är: ",kontrollSumma(sidlista)," (differansen från 1 pga. flyttalsfel)")
 
 main()
